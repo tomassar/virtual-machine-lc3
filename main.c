6c6c2b49330286ad0ee5711ed4a9c45f7d438492
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-09 16:28:25 -0400

    feat: intruction set

--- main.c
+++ main.c
@@ -17,3 +17,23 @@ enum
 };
 
 uint16_t reg[R_COUNT];
+
+enum
+{
+    OP_BR = 0, //branch
+    OP_ADD, //add
+    OP_LD, //load
+    OP_ST, //store
+    OP_JSR, //jump register
+    OP_AND, // bitewise and
+    OP_LDR, // load register
+    OP_STR, // store register
+    OP_RTI, //unused
+    OP_NOT, //bitewise not
+    OP_LDI, //load indirect
+    OP_STI, //store indirect
+    OP_JMP, //jump
+    OP_RES, //reserved (unsued)
+    OP_LEA, //load effective address
+    OP_TRAP //execute trap
+};
