babdf2167af4df41e7d9829c49b0e0d75e368a2b
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-15 18:27:57 -0400

    feat: read and write from memory and linux specific helper functions

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
 
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
@@ -60,6 +66,12 @@ enum
     TRAP_HALT = 0x25 //halt the program
 };
 
+enum
+{
+    MR_KBSR = 0xFE00, //keyboard status
+    MR_KBDR = 0xFE02
+};
+
 int read_image(const char* image_path)
 {
     return 1;
@@ -102,18 +114,53 @@ void handle_interrupt(int signal)
 {
 };
 
+struct termios original_tio;
+
 void disable_input_buffering()
 {
-};
+    tcgetattr(STDIN_FILENO, &original_tio);
+    struct termios new_tio = original_tio;
+    new_tio.c_lflag &= ~ICANON & ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+}
+
+void restore_input_buffering()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+}
+
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
+
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
 
 uint16_t mem_read(uint16_t addr)
 {
+    if (addr == MR_KBSR)
+    {
+        if( check_key() )
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }else
+        {
+            memory[MR_KBSR] = 0;
+        }
+    }
 
+    return memory[addr];
 };
 
-void mem_write(uint16_t addr, uint16_t dr)
+void mem_write(uint16_t addr, uint16_t val)
 {
-
+    memory[addr] = val;
 };
 
 uint16_t sign_extend(uint16_t x, int bit_count)
