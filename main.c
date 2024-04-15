973b32a054af454f78bd97a2c36a3c6da89c84cd
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-15 17:55:33 -0400

    feat: read images and store them in memory

--- main.c
+++ main.c
@@ -65,6 +65,39 @@ int read_image(const char* image_path)
     return 1;
 };
 
+void read_image_file(FILE* file) 
+{
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+int read_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) {return 0;};
+
+    read_image_file(file);
+
+    fclose(file);
+    return 1;
+}
+
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) & (x >> 8);
+}
+
 void handle_interrupt(int signal)
 {
 };
